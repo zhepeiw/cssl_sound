8b840263b84affc3c74bf4fe0040a67242a29d23
zhepei <zhepeiw2@illinois.edu>
2022-02-01 18:15:52 +0000

    supervised baseline clean up

--- sup_train.py
+++ sup_train.py
@@ -9,7 +9,11 @@ import datetime
 import speechbrain as sb
 from speechbrain.utils.distributed import run_on_main
 from dataset.prepare_urbansound8k import prepare_split_urbansound8k_csv
-from dataset.data_pipelines import dataio_prep
+#  from dataset.data_pipelines import dataio_prep
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import wandb
+from confusion_matrix_fig import create_cm_fig
 
 import pdb
 
@@ -45,13 +49,31 @@ class SupSoundClassifier(sb.core.Brain):
 
     def compute_objectives(self, predictions, batch, stage):
         predictions, lens = predictions  # [bs, 1, C]
-        targets, _ = batch['class']  # [bs, 1]
+        targets, _ = batch.class_string_encoded # [bs, 1]
         # TODO: augmentation
         loss = self.hparams.compute_cost(predictions, targets, lens)
-        # TODO: lr scheduler
         if hasattr(self.hparams.lr_scheduler, "on_batch_end"):
             self.hparams.lr_scheduler.on_batch_end(self.optimizer)
         # TODO: metrics
+        # Confusion matrices
+        if stage != sb.Stage.TRAIN:
+            y_true = targets.cpu().detach().numpy().squeeze(-1)
+            y_pred = predictions.cpu().detach().numpy().argmax(-1).squeeze(-1)
+
+        if stage == sb.Stage.VALID:
+            confusion_matix = confusion_matrix(
+                y_true,
+                y_pred,
+                labels=sorted(self.hparams.label_encoder.ind2lab.keys()),
+            )
+            self.valid_confusion_matrix += confusion_matix
+        if stage == sb.Stage.TEST:
+            confusion_matix = confusion_matrix(
+                y_true,
+                y_pred,
+                labels=sorted(self.hparams.label_encoder.ind2lab.keys()),
+            )
+            self.test_confusion_matrix += confusion_matix
         self.acc_metric.append(predictions, targets, lens)
         return loss
 
@@ -167,6 +189,17 @@ class SupSoundClassifier(sb.core.Brain):
             `None` during the test stage.
         """
         self.acc_metric = self.hparams.acc_metric()
+        # Confusion matrices
+        if stage == sb.Stage.VALID:
+            self.valid_confusion_matrix = np.zeros(
+                shape=(self.hparams.n_classes, self.hparams.n_classes),
+                dtype=int,
+            )
+        if stage == sb.Stage.TEST:
+            self.test_confusion_matrix = np.zeros(
+                shape=(self.hparams.n_classes, self.hparams.n_classes),
+                dtype=int,
+            )
 
     def on_stage_end(self, stage, stage_loss, epoch=None):
         """Gets called at the end of an epoch.
@@ -201,6 +234,17 @@ class SupSoundClassifier(sb.core.Brain):
             old_lr, new_lr = self.hparams.lr_scheduler(epoch)
             sb.nnet.schedulers.update_learning_rate(self.optimizer, new_lr)
             # The train_logger writes a summary to stdout and to the logfile.
+            # wandb logger
+            if self.hparams.use_wandb:
+                cm_fig = create_cm_fig(
+                    self.valid_confusion_matrix,
+                    display_labels=list(
+                        self.hparams.label_encoder.ind2lab.values()
+                    ),
+                )
+                valid_stats.update({
+                    'confusion': wandb.Image(cm_fig),
+                })
             self.hparams.train_logger.log_stats(
                 stats_meta={
                     "epoch": epoch,
@@ -217,10 +261,98 @@ class SupSoundClassifier(sb.core.Brain):
 
         # We also write statistics about test data to stdout and to the logfile.
         if stage == sb.Stage.TEST:
-            self.hparams.train_logger.log_stats(
-                stats_meta={"Epoch loaded": self.hparams.epoch_counter.current,},
-                test_stats=test_stats
+            # Per class accuracy from Test confusion matrix
+            per_class_acc_arr = np.diag(self.test_confusion_matrix) / np.sum(
+                self.test_confusion_matrix, axis=1
             )
+            per_class_acc_arr_str = "\n" + "\n".join(
+                "{:}: {:.3f}".format(class_id, class_acc)
+                for class_id, class_acc in enumerate(per_class_acc_arr)
+            )
+            # wandb logger
+            if self.hparams.use_wandb:
+                if self.hparams.use_wandb:
+                    cm_fig = create_cm_fig(
+                        self.test_confusion_matrix,
+                        display_labels=list(
+                            self.hparams.label_encoder.ind2lab.values()
+                        ),
+                    )
+                    test_stats.update({
+                        'confusion': wandb.Image(cm_fig),
+                    })
+                self.hparams.train_logger.log_stats(
+                    stats_meta={
+                        "datapoints_seen": self.hparams.datapoint_counter.current,
+                    },
+                    test_stats=test_stats
+                )
+            else:
+                self.hparams.train_logger.log_stats(
+                    {
+                        "Epoch loaded": self.hparams.epoch_counter.current,
+                        "\n Per Class Accuracy": per_class_acc_arr_str,
+                        "\n Confusion Matrix": "\n{:}\n".format(
+                            self.test_confusion_matrix
+                        ),
+                    },
+                    test_stats=test_stats,
+                )
+
+
+def dataio_prep(hparams, csv_path, label_encoder):
+    "Creates the datasets and their data processing pipelines."
+
+    config_sample_rate = hparams["sample_rate"]
+    # TODO  use SB implementation but need to make sure it give the same results as PyTorch
+    # resampler = sb.processing.speech_augmentation.Resample(orig_freq=latest_file_sr, new_freq=config_sample_rate)
+    hparams["resampler"] = torchaudio.transforms.Resample(
+        new_freq=config_sample_rate
+    )
+
+    # 2. Define audio pipeline:
+    @sb.utils.data_pipeline.takes("wav_path")
+    @sb.utils.data_pipeline.provides("sig")
+    def audio_pipeline(wav_path):
+        """Load the signal, and pass it and its length to the corruption class.
+        This is done on the CPU in the `collate_fn`."""
+
+        sig, read_sr = torchaudio.load(wav_path)
+
+        # If multi-channels, downmix it to a mono channel
+        sig = torch.squeeze(sig)
+        if len(sig.shape) > 1:
+            sig = torch.mean(sig, dim=0)
+
+        # Convert sample rate to required config_sample_rate
+        if read_sr != config_sample_rate:
+            # Re-initialize sampler if source file sample rate changed compared to last file
+            if read_sr != hparams["resampler"].orig_freq:
+                hparams["resampler"] = torchaudio.transforms.Resample(
+                    orig_freq=read_sr, new_freq=config_sample_rate
+                )
+            # Resample audio
+            sig = hparams["resampler"].forward(sig)
+
+        return sig
+
+    # 3. Define label pipeline:
+    @sb.utils.data_pipeline.takes("class_name")
+    @sb.utils.data_pipeline.provides("class_name", "class_string_encoded")
+    def label_pipeline(class_name):
+        yield class_name
+        class_string_encoded = label_encoder.encode_label_torch(class_name)
+        yield class_string_encoded
+
+    # Define datasets. We also connect the dataset with the data processing
+    # functions defined above.
+    ds = sb.dataio.dataset.DynamicItemDataset.from_csv(
+        csv_path=csv_path,
+        dynamic_items=[audio_pipeline, label_pipeline],
+        output_keys=["id", "sig", "class_string_encoded"]
+    )
+
+    return ds
 
 
 if __name__ == "__main__":
@@ -264,14 +396,27 @@ if __name__ == "__main__":
         }
     )
 
+    label_encoder = sb.dataio.encoder.CategoricalEncoder()
+    label_encoder.load_or_create(hparams['label_encoder_path'])
+    hparams["label_encoder"] = label_encoder
+
+    class_labels = list(label_encoder.ind2lab.values())
+    print("Class Labels:", class_labels)
+
     train_data = dataio_prep(
         hparams,
         os.path.join(hparams['save_folder'], 'train_task0_raw.csv'),
-        #  hparams['train_duration']
+        label_encoder,
     )
     valid_data = dataio_prep(
         hparams,
         os.path.join(hparams['save_folder'], 'valid_task0_raw.csv'),
+        label_encoder,
+    )
+    test_data = dataio_prep(
+        hparams,
+        os.path.join(hparams['save_folder'], 'test_task0_raw.csv'),
+        label_encoder,
     )
 
     brain = SupSoundClassifier(
@@ -287,5 +432,13 @@ if __name__ == "__main__":
         train_set=train_data,
         valid_set=valid_data,
         train_loader_kwargs=hparams["train_dataloader_opts"],
-        valid_loader_kwargs=hparams["train_dataloader_opts"],
+        valid_loader_kwargs=hparams["valid_dataloader_opts"],
+    )
+
+    # Load the best checkpoint for evaluation
+    test_stats = brain.evaluate(
+        test_set=test_data,
+        max_key="acc",
+        progressbar=True,
+        test_loader_kwargs=hparams["valid_dataloader_opts"],
     )
