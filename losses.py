09668f1a10eca090b5534ddb08f9f61a7a5e14bd
xi-j <xilinjiang28@gmail.com>
2022-02-19 15:22:58 -0600

    implement simclr(CL not supported yet)

--- losses.py
+++ losses.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from pytorch_metric_learning.losses import NTXentLoss
 
 class LogSoftmaxWithProbWrapper(nn.Module):
     """
@@ -46,3 +46,32 @@ class LogSoftmaxWithProbWrapper(nn.Module):
         predictions = F.log_softmax(predictions, dim=1)
         loss = self.criterion(predictions, targets) / targets.sum()
         return loss
+
+
+class SimCLRLoss(nn.Module):
+    '''
+        Loss for SimCLR
+    '''
+    def __init__(self, tau=0.5):
+        super(SimCLRLoss, self).__init__()
+        self.tau = tau
+        self.criterion = NTXentLoss(temperature=tau)
+
+    def forward(self, z1, z2):
+        """
+        Arguments
+        ---------
+        z1 : torch.Tensor (B, D)
+            Projected features of augmented examples
+        z2 : torch.Tensor (B, D)
+            Projected features of the same examples with different augmentations
+        Returns
+        ---------
+        loss : torch.Tensor 
+            Scalar NT-Xent loss
+        """
+        z_pairs = torch.cat([z1, z2], dim=0) # (2B, D)
+        indices = torch.arange(0, z1.shape[0], device=z1.device)
+        labels = torch.cat([indices, indices], dim=0)
+
+        return self.criterion(z_pairs, labels)
