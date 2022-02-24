fixing avg fgt computation

cl_table_tools.py
@@ -10,7 +10,7 @@ def compute_cl_statistics(table):
     '''
     avg_acc = np.mean(table[-1])
     forgetting = np.max(table - table[-1][None, :], axis=0)
-    avg_fgt = forgetting.mean()
+    avg_fgt = forgetting[:-1].mean()
     avg_seen_acc = np.mean(table[np.tril_indices(table.shape[0], -1)])  # off diagonal lower triangle, evaluating all past tasks
     avg_full_transfer_acc = np.mean(table[np.triu_indices(table.shape[0], 1)])
     report = {

linclf_train.py
@@ -23,6 +23,7 @@ from dataset.cl_pipeline import (
     class_balanced_dataio_prep,
 )
 from schedulers import SimSiamCosineScheduler
+from cl_table_tools import compute_cl_statistics
 
 import pdb
 
@@ -742,7 +743,6 @@ if __name__ == "__main__":
             )
             print("\n {} \n".format(cl_acc_table))
             if task_idx == num_tasks - 1:
-                from cl_table_tools import compute_cl_statistics
                 print(compute_cl_statistics(cl_acc_table))
             torch.save(
                 replay,