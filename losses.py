f73067c37aaa07e51736b7adabceb612357ecf29
xi-j <xilinjiang28@gmail.com>
2022-03-23 20:37:02 -0500

    implement BarlowTwins

--- losses.py
+++ losses.py
@@ -75,3 +75,45 @@ class SimCLRLoss(nn.Module):
         labels = torch.cat([indices, indices], dim=0)
 
         return self.criterion(z_pairs, labels)
+
+class BarlowTwinsLoss(nn.Module):
+    '''
+        Loss for Barlow Twins
+        Reference: https://github.com/facebookresearch/barlowtwins/blob/main/main.py
+    '''
+    def __init__(self, lambda_rr=5e-3, out_dim=8192, eps=1e-8, loss_scale=1.):
+        super(BarlowTwinsLoss, self).__init__()
+        self.lambda_rr = lambda_rr
+        self.out_dim = out_dim
+        self.eps = eps
+        self.loss_scale = loss_scale
+
+    @staticmethod
+    def off_diagonal(x):
+        # return a flattened view of the off-diagonal elements of a square matrix
+        n, m = x.shape
+        assert n == m
+        return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
+    def forward(self, z1, z2):
+        """
+        Arguments
+        ---------
+        z1 : torch.Tensor (B, D)
+            Projected features of augmented examples
+        z2 : torch.Tensor (B, D)
+            Projected features of the same examples with different augmentations
+        Returns
+        ---------
+        loss : torch.Tensor 
+            Scalar Barlow Twins loss
+        """ 
+        z_1_norm = (z1 - z1.mean(0)) / (z1.std(0) + self.eps)
+        z_2_norm = (z2 - z2.mean(0)) / (z2.std(0) + self.eps)
+        c = z_1_norm.T @ z_2_norm
+
+        on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
+        off_diag = self.off_diagonal(c).pow_(2).sum()
+
+        loss = on_diag + self.lambda_rr * off_diag
+        return loss / (c.shape[0]*c.shape[1]) * self.loss_scale
